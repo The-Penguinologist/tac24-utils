3161767a671a9dbbc6c4b03c01eb748daa474a52
Grandpa Joey <160619949+The-Penguinologist@users.noreply.github.com>
2026-09-19 16:19:27 +0300

    Implement messageCreate event handler

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,19 @@
+module.exports = {
+    name: 'messageCreate',
+    async execute(message) {
+        if (message.author.bot || !message.content.startsWith('!')) return;
 
+        const args = message.content.slice(1).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = message.client.prefixCommands.get(commandName);
+        if (!command) return;
+
+        try {
+            await command.execute(message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('There was an error executing that command.');
+        }
+    },
+};
